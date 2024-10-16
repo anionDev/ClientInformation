1bd55e479ff6c7703f54cc771e112f3972b7b168
Marius Göcke <marius.goecke@gmail.com>
2024-10-16 10:07:18 +0200

    added automatic updating hint of the last geo-ip-data-update-date in the readme

--- Other/Scripts/PrepareBuildCodeunits.py
+++ Other/Scripts/PrepareBuildCodeunits.py
@@ -1,7 +1,20 @@
 from pathlib import Path
+import re
 from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjectStructure
+
+
+def update_submodule_date_in_readme(repository_folder):
+    submodule_folder = GeneralUtilities.resolve_relative_path("Other/Resources/Submodules/ip-location-db", repository_folder)
+    sc: ScriptCollectionCore = ScriptCollectionCore()
+    commitdate = sc.git_get_commit_date(submodule_folder)
+    readme_file = GeneralUtilities.resolve_relative_path("./ReadMe.md", repository_folder)
+    readme_content = GeneralUtilities.read_text_from_file(readme_file)
+    date_regex = "The last update-date of the geo-ip-data is \\d\\d\\d\\d-\\d\\d-\\d\\d."
+    GeneralUtilities.assert_condition(0 < len(re.findall(date_regex, readme_content, re.MULTILINE)), f"The readme does not contain a string matching the regex \"{date_regex}\".")
+    readme_content = re.sub(date_regex, f"The last update-date of the geo-ip-data is {commitdate.strftime('%Y-%m-%d')}.", readme_content)
+    GeneralUtilities.write_text_to_file(readme_file, readme_content)
 
 
 def update_iplocation_submodule(repository_folder):
@@ -18,6 +31,7 @@ def prepare_build_codeunits():
     repository_folder = GeneralUtilities.resolve_relative_path("../../..", current_file)
     t.ensure_certificate_authority_for_development_purposes_is_generated(current_file)
     update_iplocation_submodule(repository_folder)
+    update_submodule_date_in_readme(repository_folder)
 
 
 if __name__ == "__main__":
