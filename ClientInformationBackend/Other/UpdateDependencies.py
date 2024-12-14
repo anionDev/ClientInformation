f3dc44c000fc285ba4c3c4a13c608b2c8a53150e
Marius Göcke <marius.goecke@gmail.com>
2024-12-14 10:42:21 +0100

    updated submodule

--- ClientInformationBackend/Other/UpdateDependencies.py
+++ ClientInformationBackend/Other/UpdateDependencies.py
@@ -14,14 +14,13 @@ def update_data_from_submodule(codeunit_folder: str) -> None:
     geo_information_file = GeneralUtilities.resolve_relative_path("geolite2-geo-whois-asn-country/geolite2-geo-whois-asn-country-ipv4.csv", datasubmodule_folder)
     target_folder = GeneralUtilities.resolve_relative_path(f"{codeunit_name}/Data/GeoIPData.csv", codeunit_folder)
     shutil.copyfile(geo_information_file, target_folder)
-    update_data_from_submodule(repository_folder)
 
 
 def update_dependencies():
     current_file = str(Path(__file__).absolute())
-    repository_folder = GeneralUtilities.resolve_relative_path("../../..", current_file)
+    codeunit_folder = GeneralUtilities.resolve_relative_path("../..", current_file)
     TasksForCommonProjectStructure().update_dependencies_of_typical_dotnet_codeunit(current_file, 1, sys.argv)
-    update_data_from_submodule(repository_folder)
+    update_data_from_submodule(codeunit_folder)
 
 
 if __name__ == "__main__":
