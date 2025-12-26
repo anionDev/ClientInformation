7c5f9a582c16aa9516e778110a373254704b9e6f
Marius Göcke <marius.goecke@gmail.com>
2025-12-26 20:18:52 +0100

    update

--- ClientInformation/Other/OnBuildingFinished.py
+++ ClientInformation/Other/OnBuildingFinished.py
@@ -3,8 +3,8 @@ from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_Co
 
 def on_building_finished():
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
-    tf.verify_image_is_working_with_detault_arguments(None)
-
+    tf.verify_image_is_working(None,None,443,"/API/Other/Maintenance/AvailabilityCheck",True)
+ 
 if __name__ == "__main__":
     on_building_finished()
  
